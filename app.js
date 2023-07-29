50e8b446f2cb8a23c8f5ea0f2e233343bcc15469
prashantcodehub <113547581+prashantcodehub@users.noreply.github.com>
2023-07-29 18:34:25 +0530

    create

---
+++ app.js
@@ -0,0 +1,4 @@
+const user_name = document.getElementById('name');
+const user_email = document.getElementById('emailid');
+const user_Password = document.getElementById('password');
+const user_Confirm_Password = document.getElementById('password_confirm');
